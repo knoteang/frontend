99fa1c5c6f7212bf15b3c935fb248e01b5249257
pondchance <pond-2-pin@hotmail.com>
2017-12-15 01:37:26 +0700

    admin by a

--- src/Component/TableUser/TableUser.js
+++ src/Component/TableUser/TableUser.js
@@ -1,77 +1,57 @@
 import React, { Component } from 'react'
-import { Table,Grid } from 'semantic-ui-react'
+import { Table, Grid, Button, Modal, Header, Icon } from 'semantic-ui-react'
 
 class TableUser extends Component {
 
     render() {
         return (
-            <div className="tableuser" >
-            <div class="ui grid" >
-                
-            </div>
+            <Grid>
+                <Grid.Column width={4}>
+
+                </Grid.Column>
 
                 <Grid.Column width={8}>
-                    <Table celled>
+                    <Table unstackable>
                         <Table.Header>
                             <Table.Row>
-                                <Table.HeaderCell>Name</Table.HeaderCell>
-                                <Table.HeaderCell>Status</Table.HeaderCell>
-                                <Table.HeaderCell>Notes</Table.HeaderCell>
+                                <Table.HeaderCell>User</Table.HeaderCell>
+                                <Table.HeaderCell textAlign='right'>Status</Table.HeaderCell>
                             </Table.Row>
                         </Table.Header>
 
-                        <Table.Body>
+                        <Table.Body >
                             <Table.Row>
                                 <Table.Cell>John</Table.Cell>
-                                <Table.Cell>No Action</Table.Cell>
-                                <Table.Cell selectable>
-                                    <a href='#'>Edit</a>
+                                <Table.Cell textAlign='right'>
+
+                                    <Modal trigger={<Button color="red"><i class="trash outline icon"></i> Delete</Button>} basic size='small'>
+                                        <Header icon='archive' content='Archive Old Messages' />
+                                        <Modal.Content>
+                                            <p>Do you want to delete this user?</p>
+                                        </Modal.Content>
+                                        <Modal.Actions>
+                                            <Button basic color='red' inverted  >
+                                                <Icon name='remove' /> No
+                                            </Button>
+                                            <Button color='green' inverted>
+                                                <Icon name='checkmark' /> Yes
+                                            </Button>
+                                        </Modal.Actions>
+                                    </Modal>
+
                                 </Table.Cell>
                             </Table.Row>
-                            <Table.Row>
-                                <Table.Cell>Jamie</Table.Cell>
-                                <Table.Cell>Approved</Table.Cell>
-                                <Table.Cell selectable>
-                                    <a href='#'>Edit</a>
-                                </Table.Cell>
-                            </Table.Row>
-                            <Table.Row>
-                                <Table.Cell>Jill</Table.Cell>
-                                <Table.Cell>Denied</Table.Cell>
-                                <Table.Cell selectable>
-                                    <a href='#'>Edit</a>
-                                </Table.Cell>
-                            </Table.Row>
-                            <Table.Row warning>
-                                <Table.Cell>John</Table.Cell>
-                                <Table.Cell>No Action</Table.Cell>
-                                <Table.Cell selectable warning>
-                                    <a href='#'>Requires change</a>
-                                </Table.Cell>
-                            </Table.Row>
-                            <Table.Row>
-                                <Table.Cell>Jamie</Table.Cell>
-                                <Table.Cell positive>Approved</Table.Cell>
-                                <Table.Cell selectable positive>
-                                    <a href='#'>Approve</a>
-                                </Table.Cell>
-                            </Table.Row>
-                            <Table.Row>
-                                <Table.Cell>Jill</Table.Cell>
-                                <Table.Cell negative>Denied</Table.Cell>
-                                <Table.Cell selectable negative>
-                                    <a href='#'>Remove</a>
-                                </Table.Cell>
-                            </Table.Row>
+
                         </Table.Body>
+
                     </Table>
                 </Grid.Column>
 
                 <Grid.Column width={4}>
 
                 </Grid.Column>
-            </div>
-        )
+            </Grid>
+        );
     }
 }
 export default TableUser
